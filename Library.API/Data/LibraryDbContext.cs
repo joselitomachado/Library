4d8d6739aeea4f3067148c941d4b91acda2e42f5
Joselito Machado <paguelosantos@hotmail.com>
2024-05-29 08:52:24 -0300

    feat: criar tabelas no banco de dados com migration

---
+++ Library.API/Data/LibraryDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Library.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Library.API.Data;
+
+public class LibraryDbContext : DbContext
+{
+    public LibraryDbContext(DbContextOptions<LibraryDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<AuthorModel> Authors { get; set; }
+    public DbSet<BookModel> Books { get; set; }
+}
