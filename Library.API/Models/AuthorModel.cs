1b96f398c77d83867d4cb00f6573a113654595d0
Joselito Machado <paguelosantos@hotmail.com>
2024-05-29 08:53:33 -0300

    feat: add modelo do autor

---
+++ Library.API/Models/AuthorModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Library.API.Models;
+
+public class AuthorModel
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    [JsonIgnore]
+    public ICollection<BookModel> Books { get; set; } = [];
+}
