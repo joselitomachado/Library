b346f2623080fae01beab2c828bd860d6c9f0960
Joselito Machado <paguelosantos@hotmail.com>
2024-05-29 08:57:16 -0300

    feat: implementar contratos

---
+++ Library.API/Services/Author/IAuthorInterface.cs
@@ -0,0 +1,10 @@
+﻿using Library.API.Models;
+
+namespace Library.API.Services.Author;
+
+public interface IAuthorInterface
+{
+    Task<ResponseModel<List<AuthorModel>>> GetAllAuthors();
+    Task<ResponseModel<AuthorModel>> GetAuthorById(int idAuthor);
+    Task<ResponseModel<AuthorModel>> GetAuthorByIdBook(int idBook);
+}
