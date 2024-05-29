b84f40a1ee5c6d6efd3b650ca4310b08a76de642
Joselito Machado <paguelosantos@hotmail.com>
2024-05-29 08:50:37 -0300

    configurar conexão com o banco de dados

--- Library.API/Program.cs
+++ Library.API/Program.cs
@@ -7,6 +7,11 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddDbContext<LibraryDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("LibraryCs"));
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
