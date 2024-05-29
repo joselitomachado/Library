cf9d80a44f87d5611a5b5050ef9626a25863c20e
Joselito Machado <paguelosantos@hotmail.com>
2024-05-29 08:55:59 -0300

    feat: implementar metedos de buscar dos autores

---
+++ Library.API/Controllers/AuthorController.cs
@@ -0,0 +1,44 @@
+﻿using Library.API.Models;
+using Library.API.Services.Author;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Library.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AuthorController : ControllerBase
+{
+    private readonly IAuthorInterface _authorInterface;
+
+    public AuthorController(IAuthorInterface authorInterface)
+    {
+        _authorInterface = authorInterface;
+    }
+
+    [HttpGet]
+    [Route("GetAllAuthors")]
+    public async Task<ActionResult<ResponseModel<List<AuthorModel>>>> GetAllAuthors()
+    {
+        var authors = await _authorInterface.GetAllAuthors();
+
+        return Ok(authors);
+    }
+
+    [HttpGet]
+    [Route("GetAuthorById/{idAuthor}")]
+    public async Task<ActionResult<ResponseModel<AuthorModel>>> GetAuthorById(int idAuthor)
+    {
+        var author = await _authorInterface.GetAuthorById(idAuthor);
+
+        return Ok(author);
+    }
+
+    [HttpGet]
+    [Route("GetAuthorByIdBook/{idBook}")]
+    public async Task<ActionResult<ResponseModel<AuthorModel>>> GetAuthorByIdBook(int idBook)
+    {
+        var author = await _authorInterface.GetAuthorByIdBook(idBook);
+
+        return Ok(author);
+    }
+}
