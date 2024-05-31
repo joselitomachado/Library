c714f106682171f035eda564d3643a4f06d3a5d6
Joselito Machado <paguelosantos@hotmail.com>
2024-05-31 09:50:53 -0300

    feat: dto de edição

---
+++ Library.API/DTOs/Author/AuthorEditionDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Library.API.DTOs.Author;
+
+public class AuthorEditionDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+}
