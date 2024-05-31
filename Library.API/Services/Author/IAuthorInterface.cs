b0d2b0fa561933a7ae6382a1d50d4f0699689103
Joselito Machado <paguelosantos@hotmail.com>
2024-05-31 09:52:04 -0300

    feat: adicionando a interface de create, update e delete

--- Library.API/Services/Author/IAuthorInterface.cs
+++ Library.API/Services/Author/IAuthorInterface.cs
@@ -1,4 +1,5 @@
-﻿using Library.API.Models;
+﻿using Library.API.DTOs.Author;
+using Library.API.Models;
 
 namespace Library.API.Services.Author;
 
@@ -7,4 +8,7 @@ public interface IAuthorInterface
     Task<ResponseModel<List<AuthorModel>>> GetAllAuthors();
     Task<ResponseModel<AuthorModel>> GetAuthorById(int idAuthor);
     Task<ResponseModel<AuthorModel>> GetAuthorByIdBook(int idBook);
+    Task<ResponseModel<List<AuthorModel>>> CreateAuthor(AuthorCreationDto authorCreationDto);
+    Task<ResponseModel<List<AuthorModel>>> UpdateAuthor(AuthorEditionDto authorEditionDto);
+    Task<ResponseModel<List<AuthorModel>>> DeleteAuthor(int idAuthor);
 }
