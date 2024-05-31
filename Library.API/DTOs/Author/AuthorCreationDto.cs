c5e60abbc6fc261917929a899913fc948a9d609f
Joselito Machado <paguelosantos@hotmail.com>
2024-05-31 09:50:40 -0300

    feat: dto de criação

---
+++ Library.API/DTOs/Author/AuthorCreationDto.cs
@@ -0,0 +1,7 @@
+﻿namespace Library.API.DTOs.Author;
+
+public class AuthorCreationDto
+{
+    public string Name { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+}
