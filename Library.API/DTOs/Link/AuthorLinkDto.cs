431d45e95f5a0ea85783335cc13cff2435d60051
Joselito Machado <paguelosantos@hotmail.com>
2024-06-01 14:37:59 -0300

    feat: add vinculo de autor

---
+++ Library.API/DTOs/Link/AuthorLinkDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Library.API.DTOs.Link;
+
+public class AuthorLinkDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+}
