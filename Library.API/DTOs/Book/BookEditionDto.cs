d061ddc779986fca971ba4cd9dec26a6c3e47737
Joselito Machado <paguelosantos@hotmail.com>
2024-06-01 14:37:44 -0300

    feat: add dto de edição

---
+++ Library.API/DTOs/Book/BookEditionDto.cs
@@ -0,0 +1,11 @@
+﻿using Library.API.DTOs.Link;
+using Library.API.Models;
+
+namespace Library.API.DTOs.Book;
+
+public class BookEditionDto
+{
+    public int Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public AuthorLinkDto Author { get; set; }
+}
