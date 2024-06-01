a18e6019e4e7c52bfd761a0778425972c4a8fb70
Joselito Machado <paguelosantos@hotmail.com>
2024-06-01 14:39:35 -0300

    feat: add interface do livros

---
+++ Library.API/Services/Book/IBookInterface.cs
@@ -0,0 +1,14 @@
+﻿using Library.API.DTOs.Book;
+using Library.API.Models;
+
+namespace Library.API.Services.Book;
+
+public interface IBookInterface
+{
+    Task<ResponseModel<List<BookModel>>> GetAllBooks();
+    Task<ResponseModel<BookModel>> GetBookById(int idBook);
+    Task<ResponseModel<List<BookModel>>> GetBookByIdAuthor(int idAuthor);
+    Task<ResponseModel<List<BookModel>>> CreateBook(BookCreationDto bookCreationDto);
+    Task<ResponseModel<List<BookModel>>> UpdateBook(BookEditionDto bookEditionDto);
+    Task<ResponseModel<List<BookModel>>> DeleteBook(int idBook);
+}
