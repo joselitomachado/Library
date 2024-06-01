24fcaeb0a1f9d7cda5ce7c421c8ad1d8f81fc597
Joselito Machado <paguelosantos@hotmail.com>
2024-06-01 14:37:37 -0300

    feat: add dto de criação

---
+++ Library.API/DTOs/Book/BookCreationDto.cs
@@ -0,0 +1,9 @@
+﻿using Library.API.DTOs.Link;
+
+namespace Library.API.DTOs.Book;
+
+public class BookCreationDto
+{
+    public string Title { get; set; } = string.Empty;
+    public AuthorLinkDto Author { get; set; }
+}
