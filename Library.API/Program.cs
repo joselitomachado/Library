32020c0d6bfa4ad75b8a30db7af1aa5b0273a772
Joselito Machado <paguelosantos@hotmail.com>
2024-06-03 15:43:12 -0300

    config: config para documentação

--- Library.API/Program.cs
+++ Library.API/Program.cs
@@ -2,15 +2,35 @@ using Library.API.Data;
 using Library.API.Services.Author;
 using Library.API.Services.Book;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Library.API",
+        Version = "v1",
+        Contact = new OpenApiContact
+        {
+            Name = "Joselito Machado",
+            Email = "opaguelosantos@gmail.com",
+            Url = new Uri("https://www.linkedin.com/in/joselitomachado/")
+        }
+    });
+
+    var xmlFile = "Library.API.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    c.IncludeXmlComments(xmlPath);
+});
 
 builder.Services.AddScoped<IAuthorInterface, AuthorService>();
 builder.Services.AddScoped<IBookInterface, BookService>();
+
+builder.Services.AddRouting(options => options.LowercaseUrls = false);
 
 builder.Services.AddDbContext<LibraryDbContext>(options =>
 {
